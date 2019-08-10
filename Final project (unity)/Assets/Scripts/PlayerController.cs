c829d25c9b63b0d43d25e42b831493e96255cf21
sfarn <sfarn48@gmail.com>
2019-08-10 17:31:36 -0400

    Created maze script, added a player and movement

---
+++ Final project (unity)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    public float moveSpeed;
+    public float lerpSpeed;
+
+    Rigidbody2D rb;
+    Vector2 velocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        LerpMove();
+    }
+
+    void LerpMove()
+    {
+        if (Input.GetKey(upKey))
+        {
+            velocity = Vector2.Lerp(velocity, Vector2.up, lerpSpeed);
+        }
+        else if (Input.GetKey(downKey))
+        {
+            velocity = Vector2.Lerp(velocity, Vector2.down, lerpSpeed);
+        }
+        else if (Input.GetKey(leftKey))
+        {
+            velocity = Vector2.Lerp(velocity, Vector2.left, lerpSpeed);
+        }
+        else if (Input.GetKey(rightKey))
+        {
+            velocity = Vector2.Lerp(velocity, Vector2.right, lerpSpeed);
+        }
+        else
+        {
+            velocity = Vector2.Lerp(velocity, Vector2.zero, lerpSpeed);
+        }
+
+        Vector2 newPos = rb.position + velocity * moveSpeed * Time.deltaTime; // position + velocity * moveSpeed
+        rb.MovePosition(newPos);
+    }
+}
