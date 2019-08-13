5039a9e57ee8721c50664f8a7ced2ebfe129a8c2
sfarn <sfarn48@gmail.com>
2019-08-13 15:01:21 -0400

    edited speed light decreases at

---
+++ Final project (unity)/Assets/Scripts/LightDecrease.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
+
+public class LightDecrease : MonoBehaviour
+{
+    private Light2D light;
+    public float startLightRadius;
+    private float currentLightRadius;
+    public float decreaseSpeed;
+   
+    void Start()
+    {
+        light = GetComponent<Light2D>();
+        currentLightRadius = startLightRadius;
+    }
+    
+    void Update()
+    {
+        currentLightRadius=Mathf.Lerp(currentLightRadius, 0, decreaseSpeed);
+        SetLightRadius(currentLightRadius);
+    }
+
+    void SetLightRadius(float radius)
+    {
+        light.pointLightOuterRadius = radius;
+    }
+}
